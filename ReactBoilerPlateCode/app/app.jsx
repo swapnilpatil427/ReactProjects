5788ca5cb61009ff6e07cd279d93dfd827f0c642
Swapnil Patil <Swapnil Patil>
2017-01-16 20:23:24 -0800

    BoilerPlate Projects Updated

--- ReactBoilerPlateCode/app/app.jsx
+++ ReactBoilerPlateCode/app/app.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var {Route, Router, IndexRoute, hashHistory} = require ('react-router');
 ReactDOM.render(
     <h1> React Boiler Plate App </h1>, document.getElementById('app'));
