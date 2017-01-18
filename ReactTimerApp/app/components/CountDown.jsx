53a1f9aa4500b0426f6bae72e911f8060d73d88b
Swapnil Patil <Swapnil Patil>
2017-01-18 00:16:09 -0800

    CountDown zero form visible done

--- ReactTimerApp/app/components/CountDown.jsx
+++ ReactTimerApp/app/components/CountDown.jsx
@@ -27,6 +27,10 @@ var CountDown = React.createClass({
             }
         }
     },
+    componentWillUnmount : function () {
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
     startTimer : function () {
         this.timer = setInterval(() => {
             var newCount = this.state.count - 1;
@@ -34,6 +38,11 @@ var CountDown = React.createClass({
                 count : newCount >= 0 ? newCount : 0
             });
             
+            if(newCount === 0) {
+                this.setState ({
+                    countdownStatus : 'stopped'
+                })
+            }
         }, 1000);
     },
     handleSetCountDown : function (seconds) {
