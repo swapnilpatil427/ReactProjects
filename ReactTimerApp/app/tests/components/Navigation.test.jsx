780ce2aa5f1c2171395ed46c729a3b63dd13a0f2
Swapnil Patil <Swapnil Patil>
2017-01-18 01:36:23 -0800

    Timer app Completed

---
+++ ReactTimerApp/app/tests/components/Navigation.test.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Navigation = require('Navigation');
+
+describe('Navigation Component', () => {
+    it('Should Exists', () => {
+        expect(Navigation).toExist();
+    });
+
+  
+});
